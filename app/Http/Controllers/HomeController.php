4ac097263cee6e75e000caaad302d6b11025f055
Carlos Serrano <carlosclowm@gmail.com>
2022-05-15 19:39:26 -0500

    Implemented API for get products with web scrapping

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,6 +28,30 @@ class HomeController extends Controller
     public function startScript(Request $res){
         $command = escapeshellcmd('python C:\MyRuleEngine.py "'.$res->get('input').'" ');
         $output = shell_exec($command);
-        return redirect()->back()->with('message', $output);
+        $result = preg_replace('/\s+/', '+', $output);
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => 'https://serpapi.com/search.json?q=core+i9+16+ram+&location=M%C3%A9xico&hl=es&gl=mx&api_key=139c270037bcf4176540b5277e232efdad96569b1b793187c2de47bc650863a3',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+        $array = json_decode($response,true);
+        curl_close($curl);
+        if($array){
+            $Products = $array['shopping_results'];
+        }else{
+            $Products = null;
+        }
+        //var_dump($array['shopping_results'][0]['title']);
+        //return view('home', compact('Products', 'output'));
+        return view('home', compact('Products'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,25 @@
                     <input type="text" name="input" class="form-control">
                     <button type="submit" class="btn btn-primary">Analize</button>
                 </form>
+                @if(isset($Products))
+                <div class="card mb-3 align-self-center" style="max-width: 540px;">
+                    @foreach($Products as $Product)
+                      <div class="row g-0">
+                        <div class="col-md-4">
+                          <img src="{{$Product['thumbnail']}}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                          <div class="card-body">
+                            <h5 class="card-title">{{$Product['title']}}</h5>
+                            <p class="card-text">{{$Product['price']}}</p>
+                            <a href="{{$Product['link']}}" class="btn btn-success">Buy</a>
+                            <p class="card-text"><small class="text-muted">{{$Product['source']}}</small></p>
+                          </div>
+                        </div>
+                      </div>
+                    @endforeach
+                </div>
+                @endif
             </div>
         </div>
     </div>
